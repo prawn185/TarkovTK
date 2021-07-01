87c544faa525211ddbb9580e6cb97f5b949289c8
Sean <sean@seanmizen.com>
2021-07-01 23:53:14 +0100

    Big updates all round

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import discord
 from discord.ext import commands
 import mysql.connector
 from dotenv import load_dotenv
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
+
+description = '''m4x5ton Discord's best bot evr.'''
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,7 +15,6 @@ db_host = os.getenv('DB_HOST')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_database = os.getenv('DB_DATABASE')
-
 
 wipe_password = "boom"
 
@@ -38,14 +36,74 @@ async def create(ctx, name: str, discord_id = "0", ):
         database=db_database
     )
 
-    mysql_insert_query = """INSERT INTO teamkills (`name`, `discord_id`, `deaths`) 
-                            VALUES (%s, %s, %s) """
-    record = (name, discord_id, 0)
-    cursor = db.cursor()
-    cursor.execute(mysql_insert_query, record)
+    cursor = db.cursor(buffered=True)
 
-    await ctx.send(name + " added! Welcome to the Thunderdome.")
+    sql_select_query = """SELECT * FROM teamkills WHERE name = %s"""
+    cursor.execute(sql_select_query, (name,))
+    record = cursor.fetchone()
+    msg = ""
+
+    if record is None:
+        mysql_insert_query = """INSERT INTO teamkills (`name`, `discord_id`, `deaths`) 
+                                VALUES (%s, %s, %s) """
+        record = (name, discord_id, 0)
+        cursor = db.cursor()
+        cursor.execute(mysql_insert_query, record)
+        msg = name + " added! Welcome to the Thunderdome."
+    else:
+        msg = "This guy already exists"
+
+    await ctx.send(msg)
     
+    db.commit()
+    cursor.close()
+
+
+@bot.command()
+async def inject(ctx, injection_string: str):
+    #Johnny Tables
+    db = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_database
+    )
+
+    cursor = db.cursor(buffered=True)
+    cursor.execute(injection_string)
+    records = cursor.fetchall()
+    msg = ""
+
+    if records is not None:
+        ##If statement returns results, print them out
+        for row in records:
+            for item in row:
+                msg += item + " "
+            msg += "\n"
+
+    if (msg == ""):
+        msg = "Done!"
+
+    await ctx.send(msg)
+    db.commit()
+    cursor.close()
+
+
+@bot.command()
+async def remove(ctx, name: str):
+    db = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_database
+    )
+    cursor = db.cursor(buffered=True)
+
+    sql_select_query = """DELETE FROM teamkills WHERE name = %s"""
+    cursor.execute(sql_select_query, (name,))
+
+    await ctx.send("OK - done")
+
     db.commit()
     cursor.close()
 
@@ -70,6 +128,8 @@ async def add(ctx, name: str):
     cursor.execute(sql_select_query, (name,))
     record = cursor.fetchone()
 
+    msg = ""
+
     if record is not None:
 
         print(record[3])
@@ -77,9 +137,74 @@ async def add(ctx, name: str):
         new_kill = record[3] + 1
         cursor.execute(sql_update_query, (new_kill, name))
 
-        await ctx.send("TK ADDED: \n" + name + " is now on " + str(new_kill) + " Kills")
+        msg = "TK ADDED: \n" + name + " is now on " + str(new_kill) + " Kills"
     else:
-        await ctx.send("TK Failed: \n Name does not exist.")
+        msg = "TK Failed: \n Name does not exist."
+    
+    await ctx.send(msg)
+
+    db.commit()
+    cursor.close()
+
+
+@bot.command()
+async def set(ctx, name: str, num: int):
+    """Sets the score"""
+    # await ctx.message.delete()
+    db = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_database
+    )
+
+    cursor = db.cursor(buffered=True)
+
+    sql_select_query = """SELECT * FROM teamkills WHERE name = %s"""
+    cursor.execute(sql_select_query, (name,))
+    record = cursor.fetchone()
+
+    msg = ""
+
+    if record is not None:
+        cursor = db.cursor(buffered=True)
+        sql_select_query = """UPDATE teamkills SET deaths = %s WHERE name = %s"""
+        cursor.execute(sql_select_query, (num, name))
+        msg = "SCORE SET: \n" + name + " is now on " + str(num) + " Kills"
+    else:
+        msg = "Person doesn't exist, didn't do anything."
+        pass
+
+    await ctx.send(msg)
+
+    db.commit()
+    cursor.close()
+
+
+@bot.command()
+async def get(ctx, name: str):
+    """f"""
+    # await ctx.message.delete()
+    db = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_database
+    )
+    cursor = db.cursor()
+
+    sql_select_query = """SELECT * FROM teamkills WHERE name = %s """
+    cursor.execute(sql_select_query, (name,))
+    record = cursor.fetchone()
+
+    msg = ""
+
+    if record is not None:
+        msg = record[1] + " is on " + str(record[3]) + " kills."
+    else:
+        msg = "You numpty, " + name + " doesn't even exist. Use '$add " + name + "' to add them."
+    
+    await ctx.send(msg)
 
     db.commit()
     cursor.close()
@@ -115,7 +240,7 @@ async def check(ctx):
 @bot.command()
 async def wipe(ctx, password: str):
     """Wipe TK`s"""
-    # await ctx.message.delete()
+    await ctx.message.delete()
     db = mysql.connector.connect(
         host=db_host,
         user=db_user,
@@ -168,14 +293,13 @@ async def rename(ctx, name: str, new_name: str):
     db.commit()
     cursor.close()
 
+
 @bot.command()
 async def nigel(ctx):
     """Absolutely thrashes someone"""
     # await ctx.message.delete()
-    await ctx.send(test_function())
+    await ctx.send("What an absolute Nigel")
 
-def test_function():
-    return "What an absolute Nigel"
 
 @bot.command()
 async def winner(ctx):
@@ -185,7 +309,6 @@ async def winner(ctx):
         password=db_password,
         database=db_database
     )
-    print(86125)
     cursor = db.cursor()
 
     sql_select_query = """SELECT * FROM teamkills ORDER BY deaths desc LIMIT 1"""
@@ -198,6 +321,7 @@ async def winner(ctx):
 
     await ctx.send(msg)
     cursor.close()
+
 
 @bot.command()
 async def what(ctx):
