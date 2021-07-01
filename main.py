a0ea410607fa6790f1dbc3224f7aad0b5a29a7b8
Shaun Moore <shaunmoore185@gmail.com>
2021-07-01 12:08:48 +0100

    Create add check what & wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import random
+import mysql.connector
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -9,7 +9,7 @@ There are a number of utility commands being showcased here.'''
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='/tk', description=description, intents=intents)
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
 
 
 @bot.event
@@ -20,46 +20,130 @@ async def on_ready():
     print('------')
 
 
-users = [
-    ['Max', 0],
-    ['Prawn', 0],
-    ['Smizzle', 0],
-    ['Snuggles', 0],
-    ['Mona', 0],
-    ['Panda', 0],
-    ['Hedwig', 0],
-    ['Geo', 0]
-]
+# put into env
+
+# tarkov
+# Killa69!
+
+
+@bot.command()
+async def create(ctx, name: str, discord_id: str, ):
+    db = mysql.connector.connect(
+        host="localhost",
+        user="tarkov",
+        password="Killa69!",
+        database="Tarkov"
+    )
+
+    mysql_insert_query = """INSERT INTO teamkills (`name`, `discord_id`, `deaths`) 
+                            VALUES (%s, %s, %s) """
+
+    record = (name, discord_id, 0)
+
+    cursor = db.cursor()
+
+    cursor.execute(mysql_insert_query, record)
+    db.commit()
+    print(cursor.rowcount, "Record inserted successfully into teamkills table")
+    cursor.close()
+
+    await ctx.send(name + " Added!")
 
 
 @bot.command()
 async def add(ctx, name: str):
-    """Counts TK's"""
-
+    """Adds TK`s"""
+    # await ctx.message.delete()
     pinned = ""
-    msg = "Team Kills: "
+    msg = "TK ADDED: \n"
 
-    # Get pinned message
+    db = mysql.connector.connect(
+        host="localhost",
+        user="tarkov",
+        password="Killa69!",
+        database="Tarkov"
+    )
 
-    # Read Pinned Msg
+    cursor = db.cursor()
 
-    # /tkadd name
-    #pluses 1 to the message
-    #edit the pinned msg
+    sql_select_query = """SELECT * FROM teamkills WHERE name = %s"""
+    cursor.execute(sql_select_query, (name,))
+    record = cursor.fetchone()
+    sql_update_query = """UPDATE teamkills SET deaths = %s WHERE name = %s"""
+    newkill = (record[3] + 1, name)
+    cursor.execute(sql_update_query, newkill)
 
-    # ctx.channel.purge(1)
+    sql_select_query2 = """select * from teamkills where name = %s"""
+    cursor.execute(sql_select_query2, (name,))
+    updated = cursor.fetchone()
 
-    # sticky = await ctx.send(msg)
+    await ctx.send("TK ADDED: \n"+name+" is now on "+str(updated[3])+" Kills")
 
-    # pinned = await ctx.pins()
-    stick = ctx.pins()
-    print(await stick)
-    print(await stick.content())
-    # sticky = await sticky.channel.get_message(int(859939382814179339))
-    # # await sticky.pin()
+    db.commit()
 
+    cursor.close()
 
 
+@bot.command()
+async def check(ctx):
+    """Counts TK`s"""
+    # await ctx.message.delete()
+    msg = "TeamKillers: \n"
+
+    db = mysql.connector.connect(
+        host="localhost",
+        user="tarkov",
+        password="Killa69!",
+        database="Tarkov"
+    )
+
+    cursor = db.cursor()
+
+    sql_select_query = """SELECT * FROM teamkills"""
+    cursor.execute(sql_select_query)
+    records = cursor.fetchall()
+    for row in records:
+        msg += row[1]+": "+str(row[3])+"\n"
+
+    await ctx.send(msg)
+
+    db.commit()
+
+    cursor.close()
+
+@bot.command()
+async def wipe(ctx):
+    """Wipe TK`s"""
+    # await ctx.message.delete()
+    db = mysql.connector.connect(
+        host="localhost",
+        user="tarkov",
+        password="Killa69!",
+        database="Tarkov"
+    )
+
+    cursor = db.cursor()
+    sql_update_query = """UPDATE teamkills SET deaths = 0"""
+
+    cursor.execute(sql_update_query)
+
+    db.commit()
 
 
-bot.run('ODU5OTA1MTg5ODA3OTE1MDQ4.YNzfHQ.2SJ2JJnSdUOV9TzOhyoAKG94wLA')
+
+    await ctx.send("WIPEEEEEEE")
+
+    db.commit()
+
+    cursor.close()
+
+
+@bot.command()
+async def what(ctx):
+    """Wipe TK`s"""
+    await ctx.send("\nHelp:\n Create: Creates a new user $create {Name} {DiscordID}\n Add: Adds a kill $add {Name}\n "
+                   "Check: Check total $check")
+
+
+
+bot.run('000000000')
